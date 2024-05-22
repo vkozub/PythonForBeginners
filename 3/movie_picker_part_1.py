1cf52fdb81f3075f50ed0861667fcfc08207e0fd
vkozub <vsevolod.kozub@gmail.com>
2024-05-22 03:51:32 -0400

    rework part 1. add start of part 2

--- 3/movie_picker_part_1.py
+++ 3/movie_picker_part_1.py
@@ -22,33 +22,35 @@ ACTORS = {
     'Jeremy Renner': ['Mission Impossible']
 }
 
-# Part 1. If statement
-print('Part 1. If statement. Searching by Genre.\n')
+search_input = input('Search by Genre: ')
 
-genre = input('Search by Genre: ')
-
-if GENRES.get(genre, None):
-    print(f'Available Movies: {GENRES[genre]}\n')
-    movie_by_genre = input('Enter Movie: ')
-else:
-    print(f'Available Genres: {GENRES.keys()}\n')
+if search_input == 'y':
+    print(f'Available Movies: {GENRES.keys()}\n')
     genre = input('Enter Genre: ')
-    print(f'Available Movies: {GENRES[genre]}\n')
-    movie_by_genre = input('Enter Movie: ')
-
-print(f'Movie to watch: {movie_by_genre}. Genre: {genre}.\n')
-
-print('Part 1. If statement. Searching by Actor.\n')
-
-actor = input('Search by Actor: ')
-
-if ACTORS.get(actor, None):
-    print(f'Available Movies: {ACTORS[actor]}\n')
-    movie_by_actor = input('Enter Movie: ')
+    if GENRES.get(genre):
+        print(f'Available Movies: {GENRES[genre]}\n')
+        movie_by_genre = input('Enter Movie: ')
+        if movie_by_genre in GENRES[genre]:
+            print(f'Movie to watch: {movie_by_genre}. Genre: {genre}.\n')
+        else:
+            print('Movie was not found, try again.')
+    else:
+        print('Genre was not found, try again.')
+elif search_input == 'n':
+    search_input = input('Search by Actor: ')
+    if search_input == 'y':
+        print(f'Available Actors: {ACTORS.keys()}\n')
+        actor = input('Enter Actor: ')
+        if ACTORS.get(actor):
+            print(f'Available Movies: {ACTORS[actor]} with {actor}\n')
+            movie_by_actor = input('Enter Movie: ')
+            if movie_by_actor in ACTORS[actor]:
+                print(f'Movie to watch: {movie_by_actor}. Starring: {actor}.\n')
+            else:
+                print('Movie was not found, try again.')
+        else:
+            print('Actor was not found, try again.')
+    else:
+        print('There is no searching. Choose searching by Genre or Actor.')
 else:
-    print(f'Available Actors: {ACTORS.keys()}\n')
-    actor = input('Enter Actor: ')
-    print(f'Available Movies: {ACTORS[actor]} with {actor}\n')
-    movie_by_actor = input('Enter Movie: ')
-
-print(f'Movie to watch: {movie_by_actor}. Starring: {actor}.\n')
+    print('Invalid search parameter. Try "y" or "n".')
---
+++ 3/movie_picker_part_2.py
@@ -0,0 +1,18 @@
+GENRES = {
+    'comedy': ['Meet the Parents', 'Anger Management'],
+    'adventures': ['Mummy'],
+    'romantic': ['Vanilla Sky', 'Meet Joe Black'],
+    'drama': ['Meet Joe Black'],
+    'thriller': ['Vanilla Sky'],
+    'action': ['Mission Impossible']
+}
+
+CAST = {
+    'Meet the Parents': ['Robert De Niro', 'Ben Stiller'],
+    'Anger Management': ['Adam Sandler', 'Jack Nicholson'],
+    'Mummy': ['Brendan Fraser', 'Rachel Weisz'],
+    'Vanilla Sky': ['Tom Cruise', 'Penelope Cruz', 'Cameron Diaz'],
+    'Meet Joe Black': ['Brad Pitt', 'Anthony Hopkins'],
+    'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
+}
+
